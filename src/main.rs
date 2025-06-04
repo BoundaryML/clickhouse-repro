4b0e59851da6a0f2e2091671c2273ade8b7727e0
Sam Lijin <sam@boundaryml.com>
2025-06-03 20:11:00 -0700

    embed error message in comments

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,16 @@ GROUP BY event_id;
     let events = query.fetch_all::<EventRow>().await?;
     tracing::info!("rows: {}", serde_json::to_string_pretty(&events)?);
 
-    // This query fails due to the WHERE clause. Is there a way to make this work?
+    // This query fails due to the WHERE clause.
+    //
+    //    Error: bad response: Code: 47. DB::Exception: Missing columns: 'event_data.fizz' while
+    //    processing: 'SELECT event_id, argMaxMerge(event_data) AS event_data FROM
+    //    sam_test.event_agg_table WHERE event_data.fizz = 'buzz' GROUP BY event_id', required columns:
+    //    'event_id' 'event_data.fizz' 'event_data', maybe you meant: 'event_id' or 'event_data'.
+    //    (UNKNOWN_IDENTIFIER) (version 25.4.1.37073 (official build)) failed to wait for command
+    //    termination: exit status 1
+    // 
+    // Is there a way to make this query work?
     let query = client.query(
         "SELECT event_id, event_data FROM sam_test.event_view WHERE event_data.fizz = 'buzz'",
     );
